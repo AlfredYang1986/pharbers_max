17a56bc680be7ec06e560ceb72840cda6bf9670e
李偉 <850012141@qq.com>
2017-01-04 20:40:23 +0800

    结果查询页面动态获取市场列表
    结果查询页面动态获取市场列表

--- client/app/module/business/MarketsModule.scala
+++ client/app/module/business/MarketsModule.scala
@@ -1,10 +1,5 @@
 package module.business
-import com.pharbers.aqll.util.dao.from
-import com.mongodb.casbah.Imports._
 import com.pharbers.aqll.util.dao._data_connection_cores
-import play.api.libs.json.Json
-import play.api.libs.json.Json.{toJson}
-import play.api.libs.json.JsValue
 /**
   * Created by wli on 2017/1/4.
   */
