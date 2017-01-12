e7bd6f49d6bbc6cba72883350404d23b14ab0e4d
李偉 <850012141@qq.com>
2017-01-12 20:32:32 +0800

    新增了scala连接mongodb的code
    新增了scala连接mongodb的code

--- client/app/com/pharbers/aqll/util/dao/querybase.scala
+++ client/app/com/pharbers/aqll/util/dao/querybase.scala
@@ -7,156 +7,160 @@ package com.pharbers.aqll.util.dao
 
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.query.dsl.QueryExpressionObject
+import com.mongodb.MongoCredential._
 
 trait data_connection {
-    def conn_name : String
-    val _conn = MongoConnection()
-	var _conntion : Map[String, MongoCollection] = Map.empty
-	
-	def getCollection(coll_name : String) : MongoCollection = {
-		if (!_conntion.contains(coll_name)) _conntion += (coll_name -> _conn(conn_name)(coll_name))
-		
-		_conntion.get(coll_name).get
-	}
-	
-	def resetCollection(coll_name : String) : Unit = getCollection(coll_name).drop
-	
-	def isExisted(coll_name : String) : Boolean = !(getCollection(coll_name).isEmpty)
-	
-	def releaseConntions = _conntion = Map.empty
-}
+		def conn_name : String
 
-object _data_connection_cores extends data_connection {
-    
-    override def conn_name: String = "Max_Cores" 
-}
+		val server = new ServerAddress("localhost", 2017)
+		val credentials = MongoCredential.createScramSha1Credential("Pharbers", conn_name ,"Pharbers2017.".toCharArray)
+		val _conn = MongoClient(server, List(credentials))
 
-object _data_connection_basic extends data_connection {
-    
-    override def conn_name: String = "Max_Basic"
-}
+		var _conntion : Map[String, MongoCollection] = Map.empty
+		def getCollection(coll_name : String) : MongoCollection = {
+			if (!_conntion.contains(coll_name)) _conntion += (coll_name -> _conn(conn_name)(coll_name))
 
-//object _data_connection {
-//    def conn_name : String = "Max_Cores"
-//    
-//	val _conn = MongoConnection()
-//	
-//	var _conntion : Map[String, MongoCollection] = Map.empty
-//	
-//	def getCollection(coll_name : String) : MongoCollection = {
-//		if (!_conntion.contains(coll_name)) _conntion += (coll_name -> _conn(conn_name)(coll_name))
-//		
-//		_conntion.get(coll_name).get
-//	}
-//	
-//	def resetCollection(coll_name : String) : Unit = getCollection(coll_name).drop
-//	
-//	def isExisted(coll_name : String) : Boolean = !(getCollection(coll_name).isEmpty)
-//	
-//	def releaseConntions = _conntion = Map.empty
-//}
-
-trait IDatabaseContext {
-	var coll_name : String = null
-	
-	protected def openConnection(implicit dbc: data_connection) : MongoCollection = 
-	    dbc._conn(dbc.conn_name)(coll_name)
-//	    new _data_connection2()._conn(new _data_connection2().conn_name)(coll_name)
-//	  	_data_connection._conn(_data_connection.conn_name)(coll_name)
-	    
-	protected def closeConnection = null
-}
-
-class ALINQ[T] {
-	var w : T => Boolean = x => true
-	var ls : List[T] = Nil
-  
-	def in(l: List[T]) : ALINQ[T] = {
-		ls = l
-		this
-	}
-
-	def where(f: T => Boolean) : ALINQ[T] = {
-		w = f
-		this
-	}
-	
-	def select[U](cr: (T) => U) : IQueryable[U] = {
-		var nc = new Linq_List[U]
-		for (i <- ls) {
-			if (w(i)) nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
+			_conntion.get(coll_name).get
 		}
-		nc
+
+		def resetCollection(coll_name : String) : Unit = getCollection(coll_name).drop
+
+		def isExisted(coll_name : String) : Boolean = !(getCollection(coll_name).isEmpty)
+
+		def releaseConntions = _conntion = Map.empty
 	}
-	
-	def contains : Boolean = {
-		for (i <- ls) {
-			if (w(i)) true
+
+	object _data_connection_cores extends data_connection {
+
+			override def conn_name: String = "Max_Cores"
+	}
+
+	object _data_connection_basic extends data_connection {
+
+			override def conn_name: String = "Max_Basic"
+	}
+
+	//object _data_connection {
+	//    def conn_name : String = "Max_Cores"
+	//
+	//	val _conn = MongoConnection()
+	//
+	//	var _conntion : Map[String, MongoCollection] = Map.empty
+	//
+	//	def getCollection(coll_name : String) : MongoCollection = {
+	//		if (!_conntion.contains(coll_name)) _conntion += (coll_name -> _conn(conn_name)(coll_name))
+	//
+	//		_conntion.get(coll_name).get
+	//	}
+	//
+	//	def resetCollection(coll_name : String) : Unit = getCollection(coll_name).drop
+	//
+	//	def isExisted(coll_name : String) : Boolean = !(getCollection(coll_name).isEmpty)
+	//
+	//	def releaseConntions = _conntion = Map.empty
+	//}
+
+	trait IDatabaseContext {
+		var coll_name : String = null
+
+		protected def openConnection(implicit dbc: data_connection) : MongoCollection =
+				dbc._conn(dbc.conn_name)(coll_name)
+	//	    new _data_connection2()._conn(new _data_connection2().conn_name)(coll_name)
+	//	  	_data_connection._conn(_data_connection.conn_name)(coll_name)
+
+		protected def closeConnection = null
+	}
+
+	class ALINQ[T] {
+		var w : T => Boolean = x => true
+		var ls : List[T] = Nil
+
+		def in(l: List[T]) : ALINQ[T] = {
+			ls = l
+			this
 		}
-		false
-	}
-	
-	def count : Int = ls.count(w)
-}
 
-object from {
-	def apply[T]() : ALINQ[T] = new ALINQ[T]
-	def db() : AMongoDBLINQ = new AMongoDBLINQ
-}
+		def where(f: T => Boolean) : ALINQ[T] = {
+			w = f
+			this
+		}
 
-class AMongoDBLINQ extends IDatabaseContext {
-	var w : DBObject = null
-  
-	def in(l: String) : AMongoDBLINQ = {
-		coll_name = l
-		this
-	}
-
-	def where(args: Any* ) : AMongoDBLINQ = {
-		w = new MongoDBObject
-		for (arg <- args) {
-			arg match {
-			  case a: (String, AnyRef) => w += a
-			  case a: DBObject => w = w ++ a
-			  case _ => w
+		def select[U](cr: (T) => U) : IQueryable[U] = {
+			var nc = new Linq_List[U]
+			for (i <- ls) {
+				if (w(i)) nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
 			}
+			nc
 		}
-		this
-	}
-	
-	def select[U](cr: (MongoDBObject) => U)(implicit dbc: data_connection) : IQueryable[U] = {
-		val mongoColl = openConnection
-		val ct = mongoColl.find(w)
-		var nc = new Linq_List[U]
-		for (i <- ct) {
-			nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
+
+		def contains : Boolean = {
+			for (i <- ls) {
+				if (w(i)) true
+			}
+			false
 		}
-		nc
+
+		def count : Int = ls.count(w)
 	}
 
-	def contains(implicit dbc: data_connection) : Boolean = {
-		!(select (x => x).empty)
-	}
-	
-	def selectTop[U](n : Int)(o : String)(cr : (MongoDBObject) => U)(implicit dbc: data_connection) : IQueryable[U] = {
-		val mongoColl = openConnection
-		val ct = mongoColl.find(w).sort(MongoDBObject(o -> -1)).limit(n)
-		var nc = new Linq_List[U]
-		for (i <- ct) {
-			nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
-		}
-		nc
-	}
-	
-	def selectSkipTop[U](skip : Int)(take : Int)(o : String)(cr : (MongoDBObject) => U)(implicit dbc: data_connection) : IQueryable[U] = {
-		val mongoColl = openConnection
-		val ct = mongoColl.find(w).sort(MongoDBObject(o -> -1)).skip(skip).limit(take)
-		var nc = new Linq_List[U]
-		for (i <- ct) {
-			nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
-		}
-		nc
+	object from {
+		def apply[T]() : ALINQ[T] = new ALINQ[T]
+		def db() : AMongoDBLINQ = new AMongoDBLINQ
 	}
 
-	def count(implicit dbc: data_connection) : Int = openConnection.count(w)
+	class AMongoDBLINQ extends IDatabaseContext {
+		var w : DBObject = null
+
+		def in(l: String) : AMongoDBLINQ = {
+			coll_name = l
+			this
+		}
+
+		def where(args: Any* ) : AMongoDBLINQ = {
+			w = new MongoDBObject
+			for (arg <- args) {
+				arg match {
+					case a: (String, AnyRef) => w += a
+					case a: DBObject => w = w ++ a
+					case _ => w
+				}
+			}
+			this
+		}
+
+		def select[U](cr: (MongoDBObject) => U)(implicit dbc: data_connection) : IQueryable[U] = {
+			val mongoColl = openConnection
+			val ct = mongoColl.find(w)
+			var nc = new Linq_List[U]
+			for (i <- ct) {
+				nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
+			}
+			nc
+		}
+
+		def contains(implicit dbc: data_connection) : Boolean = {
+			!(select (x => x).empty)
+		}
+
+		def selectTop[U](n : Int)(o : String)(cr : (MongoDBObject) => U)(implicit dbc: data_connection) : IQueryable[U] = {
+			val mongoColl = openConnection
+			val ct = mongoColl.find(w).sort(MongoDBObject(o -> -1)).limit(n)
+			var nc = new Linq_List[U]
+			for (i <- ct) {
+				nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
+			}
+			nc
+		}
+
+		def selectSkipTop[U](skip : Int)(take : Int)(o : String)(cr : (MongoDBObject) => U)(implicit dbc: data_connection) : IQueryable[U] = {
+			val mongoColl = openConnection
+			val ct = mongoColl.find(w).sort(MongoDBObject(o -> -1)).skip(skip).limit(take)
+			var nc = new Linq_List[U]
+			for (i <- ct) {
+				nc = (nc :+ cr(i)).asInstanceOf[Linq_List[U]]
+			}
+			nc
+		}
+
+		def count(implicit dbc: data_connection) : Int = openConnection.count(w)
 }
