a5e42eed1d3ab1128d021486a22e1f5781d25965
AlfredYang1986 <yangyuanpig@gmail.com>
2017-03-10 20:24:45 +0800

    开始拼接

--- calc/src/main/scala/com/pharbers/aqll/alcalc/almain/alMaxDriver.scala
+++ calc/src/main/scala/com/pharbers/aqll/alcalc/almain/alMaxDriver.scala
@@ -1,33 +1,37 @@
 package com.pharbers.aqll.alcalc.almain
 
-import akka.actor.{Actor, ActorLogging}
-import akka.actor.ActorContext
-import akka.actor.ActorSystem
-import akka.actor.Scheduler
+import akka.actor.{Actor, ActorContext, ActorLogging, ActorSystem, Props, Scheduler}
 import com.pharbers.aqll.alcalc.aljobs.alJob
 import com.pharbers.aqll.alcalc.aljobs.aljobtrigger._
-import com.pharbers.aqll.alcalc.aljobs.aljobtrigger.alJobTrigger.{push_max_job, schedule_jobs, finish_max_job}
+import com.pharbers.aqll.alcalc.aljobs.aljobtrigger.alJobTrigger.{finish_max_job, push_max_job, schedule_jobs}
 
 import scala.concurrent.stm.Ref
 import scala.concurrent.duration._
 
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by Alfred on 10/03/2017.
   */
+object alMaxDriver {
+    def props = Props[alMaxDriver]
+}
+
 class alMaxDriver extends Actor
-                     with ActorLogging {
+                     with ActorLogging
+                     with alMaxJobsSchedule {
 
     override def receive = {
         case push_max_job(file_path) => {
-
+            println(s"sign a job with file name $file_path")
         }
 
         case schedule_jobs() => {
-
+            println("schedule a job")
         }
 
         case finish_max_job(uuid) => {
+            println(s"finish a job with uuid $uuid")
 
         }
 
---
+++ calc/src/main/scala/com/pharbers/aqll/stub/stub_test_3.scala
@@ -0,0 +1,28 @@
+package com.pharbers.aqll.stub
+
+import akka.actor.{ActorSystem, Props}
+import akka.cluster.Cluster
+import com.pharbers.aqll.alcalc.aljobs.aljobtrigger.alJobTrigger.push_max_job
+import com.pharbers.aqll.alcalc.almain.alMaxDriver
+import com.pharbers.aqll.calc.split.{EventCollector, SplitReceptionSingleton}
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by Alfred on 10/03/2017.
+  */
+
+// test cases : 分布式Masterces
+object stub_test_3 extends App {
+    val config = ConfigFactory.load("split-master")
+    val system = ActorSystem("calc", config)
+//    val node_ip = system.settings.config.getStringList("akka.cluster.seed-nodes")
+//    val a = system.actorOf(SplitReceptionSingleton.props, SplitReceptionSingleton.name)
+    if(system.settings.config.getStringList("akka.cluster.roles").contains("splitmaster")) {
+        Cluster(system).registerOnMemberUp {
+            val a = system.actorOf(alMaxDriver.props)
+            println(a)
+            println("cluster ready")
+            a ! push_max_job("""config/new_test/2016-01.xlsx""")
+        }
+    }
+}
