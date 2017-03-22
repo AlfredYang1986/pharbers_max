e040f5df58ef42f3a39b412bc51221b354920375
李偉 <850012141@qq.com>
2017-03-23 02:04:04 +0800

    update python pipe write info.
    update python pipe write info.

--- calc/src/main/scala/com/pharbers/aqll/alcalc/alcmd/pyshell/pyShell.scala
+++ calc/src/main/scala/com/pharbers/aqll/alcalc/alcmd/pyshell/pyShell.scala
@@ -5,9 +5,8 @@ import com.pharbers.aqll.alcalc.alcmd.pyshell.PythonConfig
 /**
   * Created by liwei on 2017/3/22.
   */
-import com.pharbers.aqll.alcalc.alcmd.shellCmdExce
+import com.pharbers.aqll.alcalc.alcmd.pyshell.ShellCmdPyExce
 
-case class pyShell(val company : String,val files : List[(String,String)],val year : String) extends shellCmdExce {
-  val cmd = "python " +
-    PythonConfig(company,files,year).toArgs
+case class pyShell(val company : String,val files : List[(String,String)],val year : String) extends ShellCmdPyExce {
+  val cmd = "python " + PythonConfig(company,files,year).toArgs
 }
