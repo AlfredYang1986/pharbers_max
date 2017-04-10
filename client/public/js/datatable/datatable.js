3b9d72ba3a40235b4cd9abff37dcfda720a00131
钱鹏 <891366402@qq.com>
2017-04-10 11:42:49 +0800

    解决多次加载后分页失效问题

--- client/public/js/datatable/datatable.js
+++ client/public/js/datatable/datatable.js
@@ -2,6 +2,7 @@ var dataTableAjax = function() {
 	$('#dataTables-example').DataTable({
 		dom : '<"html5buttons"B>lTfgitp',
 		bFilter : false,
+        destroy: true,
 		bLengthChange : false,
 		buttons : []
 	});
