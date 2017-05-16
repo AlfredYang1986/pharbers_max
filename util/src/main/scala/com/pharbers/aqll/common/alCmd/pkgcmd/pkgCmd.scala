e19b1eee92c50438010095e03a94d604d3880c59
李偉 <850012141@qq.com>
2017-05-16 10:32:42 +0800

    编译报错调整。
    编译报错调整。

--- util/src/main/scala/com/pharbers/aqll/common/alCmd/pkgcmd/pkgCmd.scala
+++ util/src/main/scala/com/pharbers/aqll/common/alCmd/pkgcmd/pkgCmd.scala
@@ -1,11 +1,10 @@
 package com.pharbers.aqll.common.alCmd.pkgcmd
 
-import com.pharbers.aqll.common.alCmd.ShellOtherCmdExce
 import com.pharbers.aqll.common.alCmd.alCallShellCmdExce.otherFactory
 /**
   * Created by Alfred on 09/03/2017.
   */
-case class pkgCmd(val lst : List[String], val compress_file : String) extends ShellOtherCmdExce {
+case class pkgCmd(val lst : List[String], val compress_file : String) {
    val cmd = s"tar -czvf ${compress_file}.tar.gz ${lst.head}"
    otherFactory.CreateShellCmdExce().excute(cmd)
 }
--- util/src/main/scala/com/pharbers/aqll/common/alCmd/pycmd/pyCmd.scala
+++ util/src/main/scala/com/pharbers/aqll/common/alCmd/pycmd/pyCmd.scala
@@ -1,11 +1,10 @@
 package com.pharbers.aqll.common.alCmd.pycmd
 
-import com.pharbers.aqll.common.alCmd.ShellPythonCmdExce
 import com.pharbers.aqll.common.alCmd.alCallShellCmdExce.pythonFactory
 /**
   * Created by qianpeng on 2017/5/13.
   */
-case class pyCmd(pyDir: String, pyFileName: String, args: String, yms: String) extends ShellPythonCmdExce{
+case class pyCmd(pyDir: String, pyFileName: String, args: String, yms: String) {
 	val cmd = "python" + PyConfig(pyDir, pyFileName, Some(args), yms).toArgs
 	pythonFactory.CreateShellCmdExce().excute(cmd)
 }
