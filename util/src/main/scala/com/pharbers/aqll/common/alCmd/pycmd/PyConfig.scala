171c03bb2affc95fce8f1e973f28ab6258c8023a
钱鹏 <891366402@qq.com>
2017-05-16 20:27:30 +0800

    更改alCmd下的pycmd的判断Bug

--- util/src/main/scala/com/pharbers/aqll/common/alCmd/pycmd/PyConfig.scala
+++ util/src/main/scala/com/pharbers/aqll/common/alCmd/pycmd/PyConfig.scala
@@ -2,16 +2,10 @@ package com.pharbers.aqll.common.alCmd.pycmd
 
 case class PyConfig(pyDir: String,
                     pyFileName: String,
-                    args: Option[String] = None,
-                    yms: String) {
+                    args: Option[String] = None) {
     def toArgs: String = {
-        (if(pyDir.isEmpty) ""
-        else pyDir) +
-        (if(pyFileName.isEmpty) ""
-        else pyFileName) + " "
-        (if(args.isEmpty) ""
-        else args.get) +
-        (if(yms.isEmpty) ""
-        else yms)
+        ((if(pyDir.isEmpty) "" else pyDir) +
+        (if(pyFileName.isEmpty) "" else pyFileName) + " " +
+        (if(args.isEmpty) "" else args.get))
     }
 }
