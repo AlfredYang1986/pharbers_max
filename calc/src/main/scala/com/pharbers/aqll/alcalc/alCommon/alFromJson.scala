0eec3a1c06b0f6c3292b4e7dfe9ef9dbb844d07f
钱鹏 <891366402@qq.com>
2017-05-16 18:59:33 +0800

    错误修正

--- calc/src/main/scala/com/pharbers/aqll/alcalc/alCommon/alFromJson.scala
+++ calc/src/main/scala/com/pharbers/aqll/alcalc/alCommon/alFromJson.scala
@@ -1,11 +1,12 @@
 package com.pharbers.aqll.alcalc.alCommon
 
 import com.google.gson.Gson
+import scala.collection.mutable.Map
 
 /**
   * Created by qianpeng on 2017/5/16.
   */
-object alFromJson {
+object alFromJson2 {
 	def apply(resp: String) = formJson(resp)
-	def formJson(resp: String) = new Gson().fromJson(resp, classOf[Map[String, String]])
+	def formJson(resp: String): Map[String, String] = new Gson().fromJson(resp, classOf[Map[String, String]])
 }
