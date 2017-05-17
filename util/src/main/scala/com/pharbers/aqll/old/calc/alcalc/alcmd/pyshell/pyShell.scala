40396f0045734e2d180c61de3e61066872a2a765
钱鹏 <891366402@qq.com>
2017-05-17 12:31:50 +0800

    修改alCmd的code，删除old文件夹

--- util/src/main/scala/com/pharbers/aqll/old/calc/alcalc/alcmd/pyshell/pyShell.scala
+++
@@ -1,9 +0,0 @@
-package com.pharbers.aqll.old.calc.alcalc.alcmd.pyshell
-
-/**
-  * Created by liwei on 2017/3/22.
-  */
-
-case class pyShell(val company : String,val filename : String,yms : String) extends ShellCmdPyExce {
-  val cmd = "python " + PythonConfig(company,filename,yms).toArgs
-}
