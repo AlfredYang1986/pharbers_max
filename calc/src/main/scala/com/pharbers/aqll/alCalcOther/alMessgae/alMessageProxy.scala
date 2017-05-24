6d67db3cf05de42a60427b952be0664e87a9416d
钱鹏 <891366402@qq.com>
2017-05-24 16:31:31 +0800

    MCAL-3 重构环信
    1. 新增alErrorCode的错误
    2. 使用proxy设计模式重构环信，以后更换其他第三方的只需修改核心调用不需要动

---
+++ calc/src/main/scala/com/pharbers/aqll/alCalcOther/alMessgae/alMessageProxy.scala
@@ -0,0 +1,29 @@
+package com.pharbers.aqll.alCalcOther.alMessgae
+
+import com.pharbers.aqll.alCalcOther.alLog.alLoggerMsgTrait
+import com.pharbers.aqll.common.alErrorCode.alErrorCode
+import io.swagger.client.ApiException
+import io.swagger.client.model.{Msg, MsgContent, UserName}
+
+/**
+  * Created by qianpeng on 2017/5/24.
+  */
+class alMessageProxy extends alMessage with alLoggerMsgTrait {
+	
+	override def sendMsg(toUser: String, content: String, ext: Map[String, String] = Map.empty): Boolean = {
+		try {
+			val userName = new UserName()
+			userName.add(toUser)
+			invokeEasemobAPI(new Msg().from("project").target(userName).targetType("users").msg(new MsgContent().`type`(MsgContent.TypeEnum.TXT).msg(content)).ext(ext))
+			true
+		} catch {
+			case e: ApiException =>
+				e.getCode match {
+					case 401 => logger.error(alErrorCode.errorToJson("error client secre").toString())
+					case 500 => logger.error(alErrorCode.errorToJson("error server").toString())
+					case _ => logger.error(alErrorCode.errorToJson("unknow error").toString())
+				}
+				false
+		}
+	}
+}
