15e8be1134b6cf16f63fb79ad22e92c249f9b15c
钱鹏 <891366402@qq.com>
2017-06-04 08:33:30 +0800

    新增初步异常处理
    写入logger

--- calc/src/main/scala/com/pharbers/aqll/alCalcMemory/alexception/alException.scala
+++ calc/src/main/scala/com/pharbers/aqll/alCalcMemory/alexception/alException.scala
@@ -1,8 +1,11 @@
 package com.pharbers.aqll.alCalcMemory.alexception
 
+import com.pharbers.aqll.alCalcOther.alLog.alLoggerMsgTrait
+import play.api.libs.json.JsValue
+
 /**
   * Created by qianpeng on 2017/6/4.
   */
-class alException {
-	
+case class alException(error: JsValue) extends alLoggerMsgTrait {
+	logger.error(error.toString())
 }
