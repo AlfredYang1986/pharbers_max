25d7223a51adea30966d90d47f9efad56b0584b7
张弛 <778900559@qq.com>
2017-06-06 14:38:59 +0800

    前端功能全部实现，之后继续完善

--- client/ember_web/app/controllers/resultquery.js
+++ client/ember_web/app/controllers/resultquery.js
@@ -2,16 +2,8 @@ import Ember from 'ember';
 const { computed } = Ember;
 
 export default Ember.Controller.extend({
-    isAdmin: computed(function(){
-        let binaryAdmin = Ember.$.cookie('is_administrator');
-        if(binaryAdmin==1){
-            return true;
-        }
-        return false;
-    }),
-
     markets: computed(function(){
-        let str = Ember.$.cookie('fu_data_markets');
+        let str = Ember.$.cookie('rq_data_markets');
         str = str.replace('[', '').replace(']', '').replace(/\"/g, "");
         let arr = str.split(',');
         return arr;
