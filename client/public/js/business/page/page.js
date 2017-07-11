211e90b5b8792685908372f4af406284aa90f182
钱鹏 <891366402@qq.com>
2017-07-11 10:53:30 +0800

    解决数据查询在无数据的情况下还有分页，修改数据库的加载，由于调试Python，涉及到python的code没有提交

--- client/public/js/business/page/page.js
+++ client/public/js/business/page/page.js
@@ -1,13 +1,18 @@
 function Page(data) {
-	var page = data.result.result.page[0];
-	$('div[id="pageinfo"]').html("显示第 " + page.ROW_START + " 至 " + page.ROW_END + " 条记录，共 " + page.TOTLE_RECORD + " 条记录");
-	var element = $('#pageview');
-	options = {
-		size: "small",
-		bootstrapMajorVersion: 3,
-		currentPage: page.PAGE_CURRE,
-		numberOfPages: 5,
-		totalPages: page.TOTLE_PAGE
-	};
-	element.bootstrapPaginator(options);
+    var options, element = $('#pageview');
+	try {
+        element.show()
+        var page = data.result.result.page[0];
+        $('div[id="pageinfo"]').html("显示第 " + page.ROW_START + " 至 " + page.ROW_END + " 条记录，共 " + page.TOTLE_RECORD + " 条记录");
+        options = {
+            size: "small",
+            bootstrapMajorVersion: 3,
+            currentPage: page.PAGE_CURRE,
+            numberOfPages: 5,
+            totalPages: page.TOTLE_PAGE
+        };
+        element.bootstrapPaginator(options);
+	}catch(ex) {
+        element.hide()
+	}
 }
