7f961a4d99035834189f2a1491fdb3e5199b16e5
钱鹏 <891366402@qq.com>
2017-07-17 19:20:59 +0800

    新Max页面流程

--- calc/src/main/scala/test/Tets.scala
+++ calc/src/main/scala/test/Tets.scala
@@ -1,8 +1,9 @@
 package test
 
+import com.pharbers.aqll.common.alEncryption.alEncryptionOpt._
 /**
   * Created by qianpeng on 2017/6/12.
   */
-object Tets {
-	
+object Test extends App{
+	println(md5("demo"))
 }
