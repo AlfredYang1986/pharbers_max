9e79e64d8f1ef145cb51f71eb279032657a47e66
钱鹏 <891366402@qq.com>
2017-09-21 20:22:06 +0800

    Error

--- client/app/module/phonecode/PhoneCodeModule.scala
+++ client/app/module/phonecode/PhoneCodeModule.scala
@@ -6,11 +6,12 @@ import com.pharbers.aqll.common.sercurity.Sercurity
 import com.pharbers.bmmessages.{CommonModules, MessageDefines}
 import com.pharbers.bmpattern.ModuleTrait
 import com.pharbers.dbManagerTrait.dbInstanceManager
+import module.phonecode.PhoneCodeData.PhoneCodeData
 import module.phonecode.PhoneCodeMessages._
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
 
-object PhoneCodeModule extends ModuleTrait{
+object PhoneCodeModule extends ModuleTrait with PhoneCodeData {
 	
 	def dispatchMsg(msg : MessageDefines)(pr : Option[Map[String, JsValue]])(implicit cm : CommonModules) : (Option[Map[String, JsValue]], Option[JsValue]) = msg match {
 		case msg_send_sms_code(data) => sendSMSCode(data)(pr)
@@ -24,11 +25,19 @@ object PhoneCodeModule extends ModuleTrait{
 		implicit val db = conn.queryDBInstance("cli").get
 		try {
 			val token = pr.map(x => x.get("auth").get).getOrElse(throw new Exception("phone error"))
-			(Some(Map("result" -> toJson("success"))), None)
-//			val o : DBObject = m2d(token)
-			
+			val o : DBObject = m2d(token)
+			val phone = (token \ "phone").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+			val condition = DBObject("phone" -> phone)
+			db.queryObject(condition, "phonecode") match {
+				case None => db.insertObject(o, "phonecode", "phone")
+				case Some(one) => db.updateObject(o, "phonecode", "phone")
+			}
+			val result = toJson(d2m(o))
+			(Some(Map("reg" -> result)), None)
 		} catch {
-			case ex : Exception => (None, Some(ErrorCode.errorToJson(ex.getMessage)))
+			case ex : Exception =>
+				println(ex)
+				(None, Some(ErrorCode.errorToJson(ex.getMessage)))
 		}
 	}
 	
