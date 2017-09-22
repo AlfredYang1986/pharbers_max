c49cf34ce75c72c377146a9d7ce94241f21e43ef
YangMei <YangMei_1995@outlook.com>
2017-09-22 14:58:30 +0800

    身份验证弹出层

--- client/public/js/login/nomal.js
+++ client/public/js/login/nomal.js
@@ -4,3 +4,23 @@
 $(document).ready(function(){
     $('#popLayer').hide();
 })
+
+var openTextValidate = function(){
+    var index = layer.open({
+        type: 1,
+        skin: 'layui-layer-demo',
+        area: '600px',
+        shade: [0.8, '#393D49'],
+        content: $('#popTextValidate'),
+        cancel: function(index, layero){
+            layer.close(index)
+            $('#popTextValidate').hide();
+        }
+    });
+    layer.title("身份验证", index)
+    layer.style(index, {
+        width: '600px',
+        top: '10px',
+        color: '#fff'
+    });
+}
