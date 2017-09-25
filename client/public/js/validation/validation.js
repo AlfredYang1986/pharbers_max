a2670d4c21e8d485d836a5c756671370e2048dfa
YangMei <YangMei_1995@outlook.com>
2017-09-25 10:00:06 +0800

    更改input 验证

--- client/public/js/validation/validation.js
+++ client/public/js/validation/validation.js
@@ -10,8 +10,12 @@ if(typeof validateType  == "undefined"){
 var validateEmail = function (idName, errMes) {
     var regexp =/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
     var email = $('#'+idName).val();
+    console.log(email)
     var res = regexp.test(email);
-    if(res) return [true, "ok"];
+    console.log(res)
+    if(res) {
+        return [true, "ok"];
+    }
     else return [false, errMes];
 }
 
@@ -29,24 +33,49 @@ var formIsEmpty = function (idName) {
     else return[true, "ok"]
 }
 
-var warning = function (idName, res) {
-    var elem = $('#' + idName)
-    if(res[0] == false){
-        elem.addClass('input_alert');
-        elem.next(".form_tip_bottom").text(res[1]);
+var changeClass = function (old, newOne, idName) {
+    var elem = $('#'+idName)
+    if(elem.hasClass(old)){
+
+        elem.removeClass(old);
+        elem.addClass(newOne);
+    }else {
+        console.log(elem)
+        elem.addClass(newOne)
     }
 }
 
-var postValidation = function (idName, validateType) {
+var dealInfo = function (idName, res, succClass,errClass) {
+    var elem = $('#' + idName);
+    console.log(res)
+    if(res[0] == false){
+        console.log("sad")
+        changeClass(succClass, errClass, idName)
+        elem.next(".form_tip_bottom").text(res[1]);
+    }else{
+        changeClass(errClass, succClass, idName)
+        elem.next(".form_tip_bottom").text("");
+    }
+}
+
+//需要验证的 input id,  验证类型， 正确样式，错误样式
+var postValidation = function (idName, validateType,succ, err ) {
     if(validateType == "phone"){
         var res = validateMobilePhone(idName, "手机号码输入错误");
-        warning(res);
+        dealInfo(idName, res, succ, err);
     }else if(validateType == "email"){
         var res =validateEmail(idName, "邮箱输入格式错误");
-        warning(res);
+        dealInfo(idName, res, succ, err);
     }else {
         var res = formIsEmpty(idName);
-        warning(res);
+        dealInfo(idName, res, succ, err);
     }
+}
+
+var input_blur = function (idName, validateType) {
+    var elem = $('#' + idName);
+    elem.blur(function () {
+        postValidation(idName, validateType, 'input_success', 'input_alert');
+    })
 }
 
