b6d8d1dfc45fef9102fb8ea0bb6be12aceb8321f
jeorch <jeorch@foxmail.com>
2017-10-26 11:39:20 +0800

    添加gossip配置 && 添加读写Bson待抽离，计算局部待细化
    添加gossip配置 && 添加读写Bson待抽离，计算局部待细化

--- calc/src/main/scala/com/pharbers/aqll/alCalcOther/alfinaldataprocess/alRestoreColl2.scala
+++ calc/src/main/scala/com/pharbers/aqll/alCalcOther/alfinaldataprocess/alRestoreColl2.scala
@@ -1,22 +1,21 @@
 package com.pharbers.aqll.alCalcOther.alfinaldataprocess
 
 import com.mongodb.casbah.commons.MongoDBObject
-import com.pharbers.aqll.common.alCmd.dbcmd.dbrestoreCmd
-import com.pharbers.aqll.common.alFileHandler.databaseConfig._
-import com.pharbers.aqll.common.alFileHandler.fileConfig._
 import com.pharbers.aqll.alCalaHelp.dbcores._
+import com.pharbers.aqll.common.alCmd.dbcmd.dbrestoreCmd2
+import com.pharbers.aqll.common.alFileHandler.databaseConfig._
 
 /**
   * Created by liwei on 2017/3/20.
   */
 
-case class alRestoreColl() {
+case class alRestoreColl2() {
 
     def apply(company : String, sub_uuids : List[String]) = {
         var isfirst : Boolean = false
         sub_uuids foreach{ x =>
 //            dbrestoreCmd(db1, company, s"$root$program$scpPath$x", dbuser, dbpwd, dbhost, dbport.toInt).excute
-            dbrestoreCmd(db1, company, "config/dumpdb/Max_Cores/" + x, dbuser, dbpwd, dbhost, dbport.toInt).excute
+            dbrestoreCmd2(db1, company, "config/dumpdb/Max_Cores/" + x, dbuser, dbpwd, dbhost, dbport.toInt).excute
 //            dbrestoreCmd(db1, company, s"$root$program$dumpdb/Max_Cores/" + x, dbuser, dbpwd, dbhost, dbport.toInt).excute
             if(!isfirst){
                 dbc.getCollection(company).createIndex(MongoDBObject("hosp_Index" -> 1))
