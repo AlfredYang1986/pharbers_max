15c5955b3c4795f4719c340d18bce9fb2e70ee93
clock <2567729201@qq.com>
2017-11-18 20:34:59 +0800

    1031:

---
+++ client/app/com/pharbers/aqll/common/TokenFilter.scala
@@ -0,0 +1,31 @@
+package com.pharbers.aqll.common
+
+import javax.inject.Inject
+import akka.stream.Materializer
+import play.api.http.HttpFilters
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+
+class Filters @Inject()(log: LoggingFilter) extends HttpFilters {
+    override def filters: Seq[EssentialFilter] = {
+        Seq(log)
+    }
+}
+
+class LoggingFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
+    def apply(nextFilter: RequestHeader => Future[Result])
+             (requestHeader: RequestHeader): Future[Result] = {
+
+        val startTime = System.currentTimeMillis
+
+        nextFilter(requestHeader).map { result =>
+
+            val endTime = System.currentTimeMillis
+            val requestTime = endTime - startTime
+
+            println(s"${requestHeader.method} ${requestHeader.uri} took ${requestTime}ms and returned ${result.header.status}")
+
+            result.withHeaders("Request-Time" -> requestTime.toString)
+        }
+    }
+}
