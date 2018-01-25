bd135457d8af31cfd2e88cead56f1d1bc12a9a82
钱鹏 <891366402@qq.com>
2018-01-25 15:57:58 +0800

    正式code

--- client/app/controllers/calcresult/CalcResultController.scala
+++ client/app/controllers/calcresult/CalcResultController.scala
@@ -31,37 +31,25 @@ trait redisController {
 		val uid = (jv \ "condition" \ "uid").asOpt[String].map(x => x).getOrElse(throw new Exception(""))
 		val company = phRedisDriver().commonDriver.hget(uid, "company").get
 		val js = (jv \ "condition").asOpt[String Map JsValue].map(x => x).getOrElse(throw new Exception(""))
-		// TODO: 测试用注释掉
-		getRedisCollections(uid).map(x => x).filter(f => f == "333").map { x =>
-			MessageRoutes(MsgCalcResultSalesVsShare(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"${company}444")) ++ js)))) :: Nil, None)
+		getRedisCollections(uid).map { x =>
+			MessageRoutes(MsgCalcResultSalesVsShare(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"$company$x")) ++ js)))) :: Nil, None)
 		}
-//		getRedisCollections(uid).map { x =>
-//			MessageRoutes(MsgCalcResultSalesVsShare(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"$company$x")) ++ js)))) :: Nil, None)
-//		}
 	}
 	
 	def paralleCurVsPreWithCity(jv: JsValue)(implicit cm: CommonModules): List[MessageRoutes] = {
 		val uid = (jv \ "condition" \ "uid").asOpt[String].map(x => x).getOrElse(throw new Exception(""))
 		val company = phRedisDriver().commonDriver.hget(uid, "company").get
 		val js = (jv \ "condition").asOpt[String Map JsValue].map(x => x).getOrElse(throw new Exception(""))
-		// TODO: 测试用注释掉
-		getRedisCollections(uid).map(x => x).filter(f => f == "333").map { x =>
-			MessageRoutes(MsgCalcResultCurVsPreWithCity(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"${company}444")) ++ js)))) :: Nil, None)
+		getRedisCollections(uid).map { x =>
+			MessageRoutes(MsgCalcResultCurVsPreWithCity(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"$company$x")) ++ js )))) :: Nil, None)
 		}
-//		getRedisCollections(uid).map { x =>
-//			MessageRoutes(MsgCalcResultCurVsPreWithCity(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"$company$x")) ++ js )))) :: Nil, None)
-//		}
 	}
 
 	def paralleCondition(jv: JsValue)(implicit cm: CommonModules): List[MessageRoutes] = {
 		val uid = (jv \ "condition" \ "uid").asOpt[String].map(x => x).getOrElse(throw new Exception(""))
 		val company = phRedisDriver().commonDriver.hget(uid, "company").get
 		val js = (jv \ "condition").asOpt[String Map JsValue].map(x => x).getOrElse(throw new Exception(""))
-		// TODO: 测试用注释掉
-		getRedisCollections(uid).map(x => x).filter(f => f == "333").map { x =>
-			MessageRoutes(MsgCalcResultCondition(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"${company}444")) ++ js)))) :: Nil, None)
-		}
-//		getRedisCollections(uid).map ( x => MessageRoutes(MsgCalcResultCondition(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"$company$x")) ++ js )))) :: Nil, None))
+		getRedisCollections(uid).map ( x => MessageRoutes(MsgCalcResultCondition(toJson(Map("condition" -> toJson(Map("table" -> toJson(s"$company$x")) ++ js )))) :: Nil, None))
 	}
 }
 
